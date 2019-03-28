077dcb50ae2c00c7315d8145584c38889f9b5102
nemflores <nemesio.flores@f1006-06-34133.bmcc.cuny.edu>
2019-03-27 20:04:03 -0400

    te

---
+++ java/js/auth.js
@@ -0,0 +1,5 @@
+/* sign up */
+const signupButton = document.getElementById('submit-sign-up');
+const signupUsername = document.getElementById('sign-up-username');
+const signupEmail = document.getElementById('sign-up-email');
+const signupPassword = document.getElementById('sign-up-password');
