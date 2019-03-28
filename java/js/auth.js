835678840763ab59031c02cd0da81454ebad2084
nemflores <nemesio.flores@f1006-06-34133.bmcc.cuny.edu>
2019-03-27 20:08:22 -0400

    ye

--- java/js/auth.js
+++ java/js/auth.js
@@ -3,3 +3,11 @@ const signupButton = document.getElementById('submit-sign-up');
 const signupUsername = document.getElementById('sign-up-username');
 const signupEmail = document.getElementById('sign-up-email');
 const signupPassword = document.getElementById('sign-up-password');
+
+function createUser() {
+    const email = signupEmail.value;
+    const password = signupPassword.value;
+    firebase.auth().createUserWithEmailAndPassword(email, password);
+}
+
+signupButton.onclick = createUser;
