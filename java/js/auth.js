01fdc3bda8108faffadf71b62e5c19c396e9cf63
nemflores <nemesio.flores@f1006-06-34133.bmcc.cuny.edu>
2019-03-27 20:31:39 -0400

    ye

--- java/js/auth.js
+++ java/js/auth.js
@@ -4,10 +4,34 @@ const signupUsername = document.getElementById('sign-up-username');
 const signupEmail = document.getElementById('sign-up-email');
 const signupPassword = document.getElementById('sign-up-password');
 
+function updateUser(credential){
+    const userInfo = {
+        displayName: signupUsername.value
+    };
+    credential.user.updateProfile(userInfo);
+}
+
 function createUser() {
     const email = signupEmail.value;
     const password = signupPassword.value;
-    firebase.auth().createUserWithEmailAndPassword(email, password);
+    const promise = firebase.auth().createUserWithEmailAndPassword(email, password);
+    promise.then(updateUser);
+    promise.catch(function(error){
+        console.log(error);
+        alert(error.message);
+    });
 }
 
 signupButton.onclick = createUser;
+
+/*log in*/
+
+const loginButton = document.getElementById('submit-login');
+const loginEmail = document.getElementById('login-email');
+const loginPassword = document.getElementById('login-password');
+
+loginButton.onlick = function() {
+    const email = loginEmail.value;
+    const password = loginPassword.value;
+    firebase.auth().signInWithEmailAndPassword(email, password);
+};
